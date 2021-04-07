4bee068ba10a6641b48a6215a52150d443509f23
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-07 10:46:18 -0600

    Complete functionality for Iteration 4

--- lib/league.rb
+++ lib/league.rb
@@ -30,11 +30,7 @@ class League
   end
 
   def most_expensive_player
-    all_players = @teams.map do |team|
-      team.players
-    end
-
-    all_players = all_players.flatten
+    all_players = get_all_players
 
     highest_salary = all_players.max_by do |player|
       player.salary
@@ -51,5 +47,40 @@ class League
     most_expensive_overall
   end
 
+  def get_all_players
+    all_players = @teams.map do |team|
+      team.players
+    end
 
+    all_players = all_players.flatten
+  end
+
+  def players_by_salary_range
+    range_hash = Hash.new
+    ranges = ["Over 0M", "Over 1M", "Over 2M", "Over 3M", "Over 4M", "Over 5M"]
+
+    ranges.each do |range|
+      range_hash[range] = []
+    end
+
+    all_players = get_all_players
+
+    all_players.each do |player|
+      if player.salary >= 5_000_000
+        range_hash["Over 5M"] << player.name
+      elsif player.salary >= 4_000_000
+        range_hash["Over 4M"] << player.name
+      elsif player.salary >= 3_000_000
+        range_hash["Over 3M"] << player.name
+      elsif player.salary >= 2_000_000
+        range_hash["Over 2M"] << player.name
+      elsif player.salary >= 1_000_000
+        range_hash["Over 1M"] << player.name
+      else
+        range_hash["Over 0M"] << player.name
+      end
+    end
+
+    range_hash
+  end
 end
--- spec/league_spec.rb
+++ spec/league_spec.rb
@@ -80,7 +80,7 @@ describe League do
 
     expect(premier.most_expensive_player).to eq ["Fernandinho"]
   end
-  
+
   it 'returns the most expensive players (2)' do
     premier = League.new("Premier League")
     roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
@@ -94,5 +94,28 @@ describe League do
     premier.add_team(manchester)
 
     expect(premier.most_expensive_player).to eq ["Fernandinho", "Kevin De Bruyne"]
+  end
+
+  it 'can group players by salary range' do
+    premier = League.new("Premier League")
+    roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
+    sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
+    richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+    jamie = Player.new({name: "Jamie Tartt", position: "Striker", salary: 1_500_000})
+    fernandinho = Player.new({name: "Fernandinho", position: "Midfielder", salary: 5_200_000})
+    de_bruyne = Player.new({name: "Kevin De Bruyne", position: "Midfielder", salary: 5_200_000})
+    manchester = Team.new("Manchester City FC", "Pep Guardiola", [jamie, fernandinho, de_bruyne])
+    premier.add_team(richmond)
+    premier.add_team(manchester)
+    expected = {
+      "Over 0M" => ["Sam Obisanya"],
+      "Over 1M" => ["Roy Kent", "Jamie Tartt"],
+      "Over 2M" => [],
+      "Over 3M" => [],
+      "Over 4M" => [],
+      "Over 5M" => ["Fernandinho", "Kevin De Bruyne"]
+    }
+
+    expect(premier.players_by_salary_range).to eq expected
   end
 end
